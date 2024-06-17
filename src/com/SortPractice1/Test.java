ec63f1dd0ff70a0169ebef06381341cb38856110
xiehang <xiehang@yonyou.com>
2024-06-17 12:28:56 +0800

    1

---
+++ src/com/SortPractice1/Test.java
@@ -0,0 +1,53 @@
+package com.SortPractice1;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.math.BigInteger;
+import java.util.*;
+
+/**
+ * @author xiehang
+ * @create 2022-04-24 23:12
+ */
+public class Test {
+    public static void main(String[] args) {
+//        int n = 18;
+////        System.out.println(n + "的二进制是:" + Integer.toBinaryString(n));
+//        System.out.println(n + "的二进制是:" + Integer.toString(n, 2));
+//        System.out.println(Integer.parseInt("10010", 2));
+//        System.out.println();
+//
+////        System.out.println(n + "的八进制是:" + Integer.toOctalString(n));
+//        System.out.println(n + "的八进制是:" + Integer.toString(n,8));
+//        System.out.println(Integer.parseInt("22", 8));
+//        System.out.println();
+//
+////        System.out.println(n + "的十六进制是:" + Integer.toHexString(n));
+//        System.out.println(n + "的十六进制是:" + Integer.toString(n,16));
+//        System.out.println(Integer.parseInt("12", 16));
+//        System.out.println();
+//        System.out.println("170的十六进制是："+Integer.toString(170, 16));
+//        System.out.println(Integer.parseInt("0xAA".substring(2), 16));
+
+//        Set<Integer> hs=new HashSet<>();
+//        boolean add = hs.add(0);
+//        System.out.println("add "+add);
+//
+//        boolean add1 = hs.add(1);
+//        boolean add2 = hs.add(1);
+//        System.out.println("add2 "+add2);
+//
+//        boolean add3 = hs.add(null);
+//        boolean add4 = hs.add(null);
+//        System.out.println("add4 "+add4);
+
+//        List<Integer> list=new ArrayList<>();
+//        boolean add = list.add(1);
+//        boolean add1 = list.add(1);
+//        boolean add2 = list.add(null);
+//        boolean add3 = list.add(null);
+//        System.out.println(13);
+        System.out.println(8%10);
+        System.out.println(8/10);
+    }
+}
